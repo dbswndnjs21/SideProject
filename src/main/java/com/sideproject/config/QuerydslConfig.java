73dd2825dd9e9cba03929aaefe6260c0c61384f1
Yijae <lullulalla9711@gmail.com>
2024-12-31 16:31:16 +0900

    fix: 경로 오류

--- src/main/java/com/sideproject/config/QuerydslConfig.java
+++ src/main/java/com/sideproject/config/QuerydslConfig.java
@@ -1,5 +1,7 @@
 package com.sideproject.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class QuerydslConfig {
-    private final EntityManager
+    private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
