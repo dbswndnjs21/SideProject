ae47011a667a11670ce5904e3744636897c96929
Yijae <lullulalla9711@gmail.com>
2024-12-31 17:05:34 +0900

    fix: controller 어노테이션 중복

--- src/main/java/com/sideproject/controller/GetStudyBoardDetailApiController.java
+++ src/main/java/com/sideproject/controller/GetStudyBoardDetailApiController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
 @RestController
 @RequestMapping("/api/get/detail")
 @RequiredArgsConstructor
