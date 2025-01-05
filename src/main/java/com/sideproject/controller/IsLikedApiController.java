27f18f1c33c11399bfbf1d4ffb6632532c3b4474
Yijae <lullulalla9711@gmail.com>
2025-01-05 19:15:50 +0900

    add: like 기능을 위한 controller 추가 (+ 유저정보 가져오는 부분 구현 필요)

--- src/main/java/com/sideproject/controller/IsLikedApiController.java
+++ src/main/java/com/sideproject/controller/IsLikedApiController.java
@@ -1,13 +1,19 @@
-//package com.sideproject.controller;
-//
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class IsLikedApiController {
-//    private final
-//
+package com.sideproject.controller;
+
+import com.sideproject.jwt.JWTUtil;
+import com.sideproject.service.IsLikedService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class IsLikedApiController {
+    private final IsLikedService isLikedService;
+    private final JWTUtil jwtUtil; // user 정보를 가져오기 위해
+
 //    @PostMapping("/likes")
-//    public
-//
-//}
+//    public Long Liked(HttpServletRequest request) {}
+
+}
