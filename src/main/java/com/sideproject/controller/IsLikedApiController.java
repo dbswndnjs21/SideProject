e099611c42e81f8a32400cb602d2eddff85da222
Yijae <lullulalla9711@gmail.com>
2025-01-10 21:18:50 +0900

    edit: todo 추가

--- src/main/java/com/sideproject/controller/IsLikedApiController.java
+++ src/main/java/com/sideproject/controller/IsLikedApiController.java
@@ -4,7 +4,6 @@ import com.sideproject.jwt.JWTUtil;
 import com.sideproject.service.IsLikedService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +15,11 @@ public class IsLikedApiController {
     private final JWTUtil jwtUtil; // user 정보를 가져오기 위해
 
     @PostMapping("/likes")
+    // TODO: 테스트 제거하고 username 클라이언트에서 받아와서 기능 동작하는지 확인
+    // TODO: 반환타입 SSE 연결할 거 생각해서 추후 수정
 //    public String Liked(HttpServletRequest request, @RequestParam("studyBoardId")Long studyBoardId) {
     public String Liked(@RequestParam("studyBoardId")Long studyBoardId) {
-        String username = "wltn";
+        String username = "wltn"; // TODO: 테스트 끝나면 삭제 -> username 클라이언트 단에서 가져와야함
 //        String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
         Long state = isLikedService.updateLike(username, studyBoardId);
@@ -28,4 +29,5 @@ public class IsLikedApiController {
         return "fail";
     }
 
+    // TODO: like취소하는 메서드도 마찬가지로 @PostMapping
 }
