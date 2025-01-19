31a7cb45d082b7cbbbf9c70df54e892b0d37d288
Yijae <lullulalla9711@gmail.com>
2025-01-19 19:13:08 +0900

    edit: 저장 방식 고민중..

--- src/main/java/com/sideproject/repository/NotificationCustomRepositoryImpl.java
+++ src/main/java/com/sideproject/repository/NotificationCustomRepositoryImpl.java
@@ -1,27 +1,27 @@
-package com.sideproject.repository;
-
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.sideproject.dto.NotificationDto;
-import com.sideproject.entity.QNotification;
-import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
-
-@RequiredArgsConstructor
-public class NotificationCustomRepositoryImpl implements NotificationCustomRepository {
-    private final JPAQueryFactory jpaQueryFactory;
-
-    private final QNotification notification = QNotification.notification;
-
-    // TODO: 알림 적재하기
-    @Override
-    @Transactional
-    public Long likedYourPost(Long userId, Long studyBoardId){
-
-        long saveNotification = jpaQueryFactory
-                .insert(notification)
-                .columns(notification.title, notification.username, notification.picUrl, notification.message, notification.isRead,
-                         notification.isDeleted)
-                .values("좋아요 알림", username, )
-    }
-}
+//package com.sideproject.repository;
+//
+//import com.querydsl.core.types.Projections;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import com.sideproject.dto.NotificationDto;
+//import com.sideproject.entity.QNotification;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@RequiredArgsConstructor
+//public class NotificationCustomRepositoryImpl implements NotificationCustomRepository {
+//    private final JPAQueryFactory jpaQueryFactory;
+//
+//    private final QNotification notification = QNotification.notification;
+//
+//    // TODO: 알림 적재하기
+//    @Override
+//    @Transactional
+//    public Long likedYourPost(Long userId, Long studyBoardId){
+//
+//        long saveNotification = jpaQueryFactory
+//                .insert(notification)
+//                .columns(notification.title, notification.username, notification.picUrl, notification.message, notification.isRead,
+//                         notification.isDeleted)
+//                .values("좋아요 알림", username, )
+//    }
+//}
