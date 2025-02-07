a814795b8f22eace42e5912ba51e6866ca2362ed
Yijae <lullulalla9711@gmail.com>
2025-02-07 19:29:04 +0900

    add: 예외 던지면 예외 발생 메서드에 대한 로그도 찍기

--- src/main/java/com/sideproject/aspect/LoggingAdvisor.java
+++ src/main/java/com/sideproject/aspect/LoggingAdvisor.java
@@ -25,11 +25,19 @@ public class LoggingAdvisor {
 
         log.info("Starting method: {}, {}", methodName, className);
 
-        Object result = joinPoint.proceed(); // 타깃 메서드 실행
+        // 예외 발생 로그 찍으려면 타깃 메서드를 실행 해봐야함
+
+        try {
+            Object result = joinPoint.proceed(); // 타깃 메서드 실행
 //        joinPoint.proceed();
 
-        log.info("Ending method: {}, {}", methodName, className);
+            log.info("Ending method: {}, {} (duration Time: {} ms)", methodName, className, duration);
 
-        return result;
+            return result;
+        } catch (Exception e) {
+            log.error("Exception 발생 method: {}, {} - Message: {}", methodName, className, e.getMessage(), e);
+
+            throw e; // 예외 다시 던지기
+        }
     }
 }
