25d3569fa44748dc678786439fa3e8f6a47e0fe2
Yijae <lullulalla9711@gmail.com>
2025-02-18 14:27:27 +0900

    add: 최근 본 post 5개씩 조회하는 메서드 추가

--- src/main/java/com/sideproject/controller/RecentViewApiController.java
+++ src/main/java/com/sideproject/controller/RecentViewApiController.java
@@ -5,20 +5,22 @@ import com.sideproject.service.RecentViewService;
 import com.sideproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/recent")
 public class RecentViewApiController {
     private final RecentViewService recentViewService;
     private final UserService userService;
 
-    @PostMapping("/recent")
+    @PostMapping
     public void addRecentView(@RequestParam("studyBoardId") Long studyBoardId) {
 //    public void addRevcentView(@RequestParam("studyBoardId") Long studyBoardId) {
 
@@ -36,4 +38,12 @@ public class RecentViewApiController {
 
         recentViewService.addPostRecentView(userId, studyBoardId);
     }
+
+    @GetMapping("/view")
+    public ResponseEntity<Set<Long>> getRecentViewPosts(@RequestParam("username") String username) {
+        Long userId = userService.getUserIdByUsername(username);
+        Set<Long> postIds = recentViewService.getViewsData(userId, 5);
+        return ResponseEntity.ok(postIds);
+    }
+
 }
